5ac0d2c694056c045e81688df22dcf8b3409b791
Marta Hilgert <marta.hil.1988@gmail.com>
2025-05-27 21:18:17 -0300

    Feat: add ProductContainer layout with styled ImageContainer and ProductDetails

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>T-shirt X</h1>
+        <span>$79.90</span>   
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi omnis sit atque ipsum dolores, eligendi nostrum. Doloribus repudiandae quod numquam quas voluptatibus quasi in, magnam quis aperiam quibusdam quisquam dignissimos.</p>     
+
+        <button>
+          Buy now
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
