c964b5b14e24672528930ec8bc34fbac312f4b60
Marta Hilgert <marta.hil.1988@gmail.com>
2025-05-29 19:17:47 -0300

    Feat: create checkout API route and start purchase flow with Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -11,10 +11,15 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -27,7 +32,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>
+        <button onClick={handleBuyProduct}>
           Buy now
         </button>
       </ProductDetails>
@@ -70,6 +75,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'USD',
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour
